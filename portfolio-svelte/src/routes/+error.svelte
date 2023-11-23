f5d93e0035f38088a4ecf1ea722f82b7c7ba0858
carlosiborra <charlie.iborra@gmail.com>
2023-11-23 14:02:18 +0100

    [DEL] Cleared

--- portfolio-svelte/src/routes/+error.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Are you lost?</h1>
-<p>It seems like you've hit a route that doesn't exist. Try going back <a href="/">home</a>.</p>
--- portfolio-svelte/src/routes/notes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    // console.log('page.svelte');
-</script>
-
-<div>
-    <h1>About</h1>
-    <p>This is the about page.</p>
-</div>
